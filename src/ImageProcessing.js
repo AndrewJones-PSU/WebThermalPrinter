46f4290bad7036b6eb7de79f156063344f2ed20f
AndrewJones-PSU <aej5218@psu.edu>
2022-12-23 01:32:50 -0500

    added image processing functionality

---
+++ src/ImageProcessing.js
@@ -0,0 +1,40 @@
+// Given a base64 image file, format it for printing
+// Formatting includes:
+// - resizing to config.img.width (default is 576 px wide)
+// - converting to black and white (NOT grayscale) using config.img.bwmethod (can be "none", "threshold", or "floyd-steinburg")
+// - converting format to PNG, if necessary
+// returned image is a base64 string
+
+// Import modules
+const config = require("../config.json");
+const marked = require("marked");
+
+async function formatImage(img) {
+	// Create a Jimp image from the base64 string
+	let image = await Jimp.read(Buffer.from(img, "base64"));
+	// Resize the image
+	image.resize(config.img.width, Jimp.AUTO);
+	// Convert the image to black and white
+	switch (config.img.bwmethod) {
+		default: // if invalid, default to grayscale
+			image = image.grayscale();
+			break;
+		case "none": // if none, just make grayscale and let the printer auto-threshold
+			image = image.grayscale();
+			break;
+		case "threshold":
+			image = image.threshold(config.img.threshold);
+			break;
+		case "floyd-steinburg":
+			image = image.dither565();
+			break;
+	}
+	// Convert the image to PNG, if necessary
+	if (image.getExtension() !== "png") {
+		image = await image.getBufferAsync(Jimp.MIME_PNG);
+	}
+	// Return the base64 string
+	return image.toString("base64");
+}
+
+module.exports = formatImage;
