a780db8aa5cc0aa3319b221fccccebea5fdc999b
AndrewJones-PSU <aej5218@psu.edu>
2022-12-25 03:16:47 -0500

    implemented processing referenced images
    images that are referenced in markdown files are now downloaded, processed for printing, and then tossed into the generated HTML for screenshotting. This isn't fully done (some HTML tags aren't preserved) but this should work fairly well for now.

--- src/TextToImage.js
+++ src/TextToImage.js
@@ -5,6 +5,8 @@ const config = require("../config.json");
 const md = require("markdown-it")();
 const puppeteer = require("puppeteer");
 const Jimp = require("jimp");
+const https = require("https");
+const ImageProcessing = require("./ImageProcessing.js");
 
 let browser;
 let page;
@@ -51,7 +53,16 @@ async function textToImage(textFile) {
 
 	// scan through the HTML and check for any images
 	// if there are images, download them, format them using ImageProcessing.js, and replace the src with the local path
-	// TODO: implement this
+	newImages = await parseAndFormatImages(html);
+	// replace the src tags in the html with data URIs
+	// Note that currently we exclude all other HTML/CSS formatting from the image tags and fix the width to 576px
+	// TODO: preserve HTML/CSS tags (this will require modifying ImageProcessing.js as well, should be simple tbh)
+	for (let i = 0; i < newImages[0].length; i++) {
+		html = html.replace(
+			newImages[1][i],
+			`<img src="${newImages[0][i].getBase64(Jimp.MIME_PNG)}" width=${config.img.width}/>`
+		);
+	}
 
 	// Render the page to a png
 	// start by setting the page content
@@ -60,4 +71,52 @@ async function textToImage(textFile) {
 	screenshot = page.screenshot({ type: "png", fullPage: true, encoding: "base64" });
 	// convert the screenshot to a jimp image and return
 	return Jimp.read(Buffer.from(screenshot, "base64"));
+}
+
+async function parseAndFormatImages(html) {
+	// scan through the HTML and check for any images
+	// if there are images, download them, format them using ImageProcessing.js, and return the Jimp images + the
+	// original HTML tags
+
+	// Start by getting all the img tags
+	let imgTags = html.match(/<img [^>]*>/g);
+
+	// map the function to get the images from the img tags
+	let mapping = imgTags.map(getImage);
+
+	// return a promise that resolves when all the images have downloaded and been formatted
+	return Promise.all(mapping).then((images) => {
+		// once the images have downloaded, format them
+		// return them + the original img tags
+		return Promise.all([images.map(ImageProcessing.formatImage), imgTags]);
+	});
+}
+
+// downloads an image from an HTML img tag and returns it's buffer in a promise
+async function getImage(imgTag) {
+	return new Promise((resolve, reject) => {
+		// Get the src attribute
+		let src = imgTag.match(/src="[^"]*"/g);
+		// download the image
+		https.get(src, (response) => {
+			// check that we actually got a response
+			if (response.statusCode !== 200) {
+				reject("Error: Invalid status code: " + response.statusCode + " (Expected 200)");
+			}
+			// check if the response is an image
+			if (response.headers["content-type"].startsWith("image/")) {
+				// if it is, download it
+				let chunks = [];
+				response.on("data", (chunk) => {
+					chunks.push(chunk);
+				});
+				response.on("end", () => {
+					resolve(Buffer.concat(chunks));
+				});
+			} else {
+				// if it isn't, reject the promise
+				reject("Error: Not an image");
+			}
+		});
+	});
 }
