f4c823f6e9737506f52678b693cc2bbb197218b6
AndrewJones-PSU <aej5218@psu.edu>
2022-12-28 23:57:56 -0500

    added multer, updated routes

--- routes/preview.js
+++ routes/preview.js
@@ -1,5 +1,13 @@
+const config = require("../config.json");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({
+	storage: storage,
+	limits: { files: config.server.maxFileCount, fileSize: config.server.maxFileSize },
+});
+
 function preview(app) {
-	app.get("/preview", (req, res) => {
+	app.get("/preview", upload.any(), (req, res) => {
 		res.status(200);
 		res.send("Preview");
 	});
--- routes/print.js
+++ routes/print.js
@@ -1,5 +1,13 @@
+const config = require("../config.json");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({
+	storage: storage,
+	limits: { files: config.server.maxFileCount, fileSize: config.server.maxFileSize },
+});
+
 function print(app) {
-	app.post("/print", (req, res) => {
+	app.post("/print", upload.any(), (req, res) => {
 		res.status(200);
 		res.send("Print");
 	});
