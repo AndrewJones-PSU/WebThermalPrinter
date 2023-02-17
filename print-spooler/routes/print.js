960dafbbce66ebaee54952477c3fad684e5baa27
AndrewJones-PSU <aej5218@psu.edu>
2023-02-17 02:42:28 -0500

    implemented spooler image validation

--- print-spooler/routes/print.js
+++ print-spooler/routes/print.js
@@ -1,5 +1,6 @@
 const config = require("../../config.json");
 const ImageValidation = require("../src/ImageValidation.js");
+const Spooler = require("../src/Spooler.js");
 
 // Print route does 2 things to the files:
 // A: It validates the files using src/ImageValidation.js
@@ -8,13 +9,19 @@ function print(app, multerUpload) {
 	app.post("/print", multerUpload.array("files"), async (req, res) => {
 		// Validate files
 		let files = req.files;
-		let validFiles = [];
+		// Validate each file, and send an error if any are invalid
 		for (let i = 0; i < files.length; i++) {
-			validFiles.push(ImageValidation.validateImage(files[i]));
+			// For each file, check if it is valid
+			if (!ImageValidation.validateImage(files[i])[0]) {
+				// If the file is invalid, send an error and return
+				res.status(400);
+				res.send(
+					"Invalid file: " + files[i].originalname + " (" + ImageValidation.validateImage(files[i])[1] + ")"
+				);
+				return;
+			}
 		}
-		validFiles = await Promise.all(validFiles);
-
-		// Add files to spooler queue
+		// If all files are valid, add them to the spooler queue
 	});
 }
 
--- print-spooler/src/ImageValidation.js
+++ print-spooler/src/ImageValidation.js
@@ -2,9 +2,52 @@
 // 1. It is a PNG image
 // 2. The image is at most config.img.width x config.img.maxheight in size
 // 3. The image is pure black and white (each pixel is either (0,0,0) or (255,255,255))
+// The function returns an array with two elements, the first being a boolean indicating whether the image is valid,
+// and the second being a string containing an error message if the image is invalid.
+
+const config = require("../config.json");
+const sizeOf = require("image-size");
 
 function validateImage(image) {
-	return new Promise((resolve, reject) => {});
+	// Check that the image is a PNG
+	if (image.mimetype !== "image/png") {
+		return [false, "Not a PNG Image"];
+	}
+	// Get image dimensions
+	let imageDims = sizeOf(image.path);
+
+	// Check that the image is the proper width and is not too tall
+	if (imageDims.width != config.img.width) {
+		return [false, "Image width is not " + config.img.width + "(given " + imageDims.width + ")"];
+	}
+	if (imageDims.height > config.img.maxheight) {
+		return [false, "Image height is greater than " + config.img.maxheight + "(given " + imageDims.height + ")"];
+	}
+
+	// Check that the image is pure black and white
+	// Start by creating a canvas and drawing the image on it
+	var canvas = document.createElement("canvas");
+	canvas.width = imageDims.width;
+	canvas.height = imageDims.height;
+	var context = canvas.getContext("2d");
+	context.drawImage(image, 0, 0);
+	var imageData = context.getImageData(0, 0, image.width, image.height);
+	var data = imageData.data;
+	// Check that each pixel is either (0,0,0) or (255,255,255)
+	for (var i = 0; i < data.length; i += 4) {
+		switch (data[i]) {
+			// If the red value is 0, check that the green and blue values are also 0, and vice versa.
+			case 0:
+				if (data[i + 1] !== 0 || data[i + 2] !== 0) return [false, "Image is not pure black and white"];
+				break;
+			case 255:
+				if (data[i + 1] !== 255 || data[i + 2] !== 255) return [false, "Image is not pure black and white"];
+				break;
+			default:
+				return [false, "Image is not pure black and white"];
+		}
+	}
+	return [true, "OK"];
 }
 
 module.exports = { validateImage };
