9e04154768cfced7136bdd5bdb6845ddd789aa29
AndrewJones-PSU <aej5218@psu.edu>
2023-02-25 02:26:54 -0500

    Spooler should have its own port def in env.json

--- print-spooler/app-spooler.js
+++ print-spooler/app-spooler.js
@@ -1,6 +1,7 @@
 // TODO: This should be an https server,
 const express = require("express");
 const config = require("./../config.json");
+const env = require("./../env.json");
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const multerUpload = multer({
@@ -14,8 +15,8 @@ app.use(express.json());
 // Dynamically import routes
 require("./routes/routeloader")(app, multerUpload);
 
-const PORT = config.server.port;
-if (!PORT) throw new Error("PORT is not defined in config.json");
+const PORT = env.spoolerPort;
+if (!PORT) throw new Error("PORT is not defined in env.json");
 app.listen(PORT, () => {
 	console.log(`Spooler is running on port ${PORT}`);
 });
